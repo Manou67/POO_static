388741971d8929a10930c6dfcebe3637e63c87eb
Manou67 <manou.buono@gmail.com>
2019-11-28 18:04:39 +0100

    quete POO 6 - static

--- Speedometer.php
+++ Speedometer.php
@@ -3,16 +3,20 @@
 
 class Speedometer
 {
-    public static function ConvertKmToMiles(int $speed)
+    const KmToMiles = 0.621;
+    const MilesToKm = 1.609;
+
+    public static function convertKmToMiles(int $speed)
     {
-        $resultMiles = $speed * 0.6215;
+        $resultMiles = $speed * self::KmToMiles;
         echo 'La conversion de ' . $speed . ' Km en Miles est la suivante : ' . $resultMiles . '.';
     }
 
     public static function ConvertMilesToKm(int $speed)
     {
-        $resultKm = $speed * 1.609;
+        $resultKm = $speed * self::MilesToKm ;
         echo 'La conversion de ' . $speed . ' Miles en Km est la suivante : ' . $resultKm . '.';
     }
 }
+
 
